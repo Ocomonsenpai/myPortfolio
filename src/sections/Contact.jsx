c628bebf088cede27c21bd31110b3e64784a2a94
Ocomonsenpai <ryantacaisan13@gmail.com>
2026-04-06 21:48:56 +0800

    Enhance social media links in Contact section with target and rel attributes for improved security and usability

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -10,8 +10,20 @@ const Contact = () => {
       <div className="info">
         <h1 className="f-name">RYAN TACAISAN</h1>
         <div className="social">
-          <a href="https://web.facebook.com/tacaisan.ryan">FACEBOOK</a>
-          <a href="https://www.linkedin.com/in/ryan-tacaisan-b56987307/">LINKEDIN</a>
+          <a
+            href="https://web.facebook.com/tacaisan.ryan"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            FACEBOOK
+          </a>
+          <a
+            href="https://www.linkedin.com/in/ryan-tacaisan-b56987307/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LINKEDIN
+          </a>
         </div>
 
         <h3 className="email">
